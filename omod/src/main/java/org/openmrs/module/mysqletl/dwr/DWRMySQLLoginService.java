93a0ef45a093b31edaad31f54f96784adca307a6
VINEET KUMAR <vineetv2821993@gmail.com>
2014-06-28 14:29:59 +0530

    Added progress bar, auto grant priveleges to mysql client,few UI change, modularity in structure

--- omod/src/main/java/org/openmrs/module/mysqletl/dwr/DWRMySQLLoginService.java
+++ omod/src/main/java/org/openmrs/module/mysqletl/dwr/DWRMySQLLoginService.java
@@ -21,60 +21,13 @@ import org.openmrs.module.mysqletl.tools.SSHClient;
 public class DWRMySQLLoginService {
 	
 	public List<String> loginMySQL(LoginParams params) throws APIException  {
-		List<String> arrayList = new ArrayList<String>();
-		try{ 
-			Class.forName("com.mysql.jdbc.Driver");
-			String connectionURL = "jdbc:mysql://"+params.gethost()+":"+params.getport()+"/";
-			Connection con = DriverManager.getConnection (connectionURL , params.getuser(), params.getpass());
-			ResultSet rs = con.getMetaData().getCatalogs();
-			while (rs.next()) {
-			    arrayList.add(rs.getString("TABLE_CAT"));
-			}
-			MySQLClient.MySQLParameters(params); //Setting MySQL Parameters for Later Use
-			return arrayList;
-		}
-		catch(Exception e){
-		    return null;
-	    }
+		return MySQLClient.login(params);
 	}
 	public List<String> getTables(LoginParams params,String db_name) throws APIException, ClassNotFoundException, SQLException  {
-	    List<String> arrayList = new ArrayList<String>();
-		try{ 
-			Class.forName("com.mysql.jdbc.Driver");
-			String connectionURL = "jdbc:mysql://"+params.gethost()+":"+params.getport()+"/"+db_name;
-			Connection con = DriverManager.getConnection (connectionURL , params.getuser(), params.getpass());
-			DatabaseMetaData md = con.getMetaData();
-			ResultSet rsTable = md.getTables(null, null, "%", null);
-			while (rsTable.next()) {
-				arrayList.add(rsTable.getString(3));
-			}
-		    return arrayList;
-		}
-		catch(Exception e){
-		    return null;
-	    }
+		return MySQLClient.getTables(params, db_name);
 	}
 	public List<String> getColumns(LoginParams params,String db_name,String table_name) throws APIException, ClassNotFoundException, SQLException  {
-	    List<String> arrayList = new ArrayList<String>();
-		try{ 
-			Class.forName("com.mysql.jdbc.Driver");
-			String connectionURL = "jdbc:mysql://"+params.gethost()+":"+params.getport()+"/"+db_name;
-			Connection con = DriverManager.getConnection (connectionURL , params.getuser(), params.getpass());
-			Statement stmt = null;
-			stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM "+table_name);
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int columnCount = rsmd.getColumnCount();
-
-			for (int i = 1; i < columnCount + 1; i++ ) {
-				arrayList.add(rsmd.getColumnName(i));
-			}
-
-		    return arrayList;
-		}
-		catch(Exception e){
-		    return null;
-	    }
+		return MySQLClient.getColumns(params, db_name, table_name);
 	}
 	public String goTransform(LoginParams params,String serverType,String db_name,String table_name,List<String> column_list) throws APIException  {
 		try{ 
@@ -103,6 +56,8 @@ public class DWRMySQLLoginService {
 			SSHClient.SetSSHParameters(params.gethost(),params.getuser(),params.getpass());
 			//Get Own IP Address which where we are client to machine running Hive and SSH
 			String Host = SSHClient.getIpAddress();
+			//grant Privileges to client IP to connect to MYSQL DB on remote machine
+			MySQLClient.grantPrivileges(Host);
 			//create database in hive
 			//HiveClient.createDatabase(params.gethost(), params.getport(), "", "", db_name);
 			//Sqoop Import Data
